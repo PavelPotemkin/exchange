c19547eaf0339569bc1db206c1ab8791f8c7cb47
p.potemkin <p.potemkin@space307.com>
2024-05-24 11:49:48 +0000

    updates

--- app.vue
+++ app.vue
@@ -8,6 +8,24 @@ const { pending, error } = await useAsyncData(async () => {
   await initExchangeWidget()
   return {}
 })
+
+const config = useRuntimeConfig()
+
+useHead({
+  title: 'Exchange C2F/F2C',
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: `${config.app.baseURL}apple-touch-icon.png` },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${config.app.baseURL}favicon-32x32.png` },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: `${config.app.baseURL}favicon-16x16.png` },
+    { rel: 'manifest', href: `${config.app.baseURL}site.webmanifest` },
+    { rel: 'mask-icon', href: `${config.app.baseURL}safari-pinned-tab.svg`, color: '#5bbad5' },
+    { rel: 'icon', type: 'image/x-icon', href: `${config.app.baseURL}favicon.ico` },
+  ],
+  meta: [
+    { name: 'msapplication-TileColor', content: '#da532c' },
+    { name: 'theme-color', content: '#ffffff' },
+  ]
+})
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,22 +8,6 @@ export default defineNuxtConfig({
     },
   },
   ssr: false,
-  app: {
-    head: {
-      title: 'Exchange C2F/F2C',
-      link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'manifest', href: '/site.webmanifest' },
-        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-      ],
-      meta: [
-        { name: 'msapplication-TileColor', content: '#da532c' },
-        { name: 'theme-color', content: '#ffffff' },
-      ],
-    },
-  },
   vite: {
     css: {
       preprocessorOptions: {
